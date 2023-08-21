35d6ac41996834b9aa55b8249fd5389ae9067296
Mehedi24434 <mehedi24434@gmail.com>
2023-08-21 04:05:07 +0000

    file system updated

--- commodities.py
+++ commodities.py
@@ -1,8 +1,16 @@
 import dynamic_function as d
+import time
 database_name='DTCC'
 collection_name = 'commodities'
 folder_path = '/home/ec2-user/Documents/DTCC_data/extracts/Commodities'
-processed_files = set()  # Initialize an empty set to store processed filenames
-resulting_dataframes, processed_files = d.read_csvs_with_dates(folder_path, processed_files)
-for df in resulting_dataframes:
-    d.insert_dataframe_to_mongodb(df,  database_name,collection_name )
+while True:
+    try:
+        processed_files = set()  # Initialize an empty set to store processed filenames
+        resulting_dataframes, processed_files = d.read_csvs_with_dates(folder_path, processed_files)
+        for df in resulting_dataframes:
+            d.insert_dataframe_to_mongodb(df,  database_name,collection_name )
+            
+    except Exception as e:
+        print(f"An exception occurred: {e}")
+        print("Retrying in 3 seconds...")
+        time.sleep(3)
